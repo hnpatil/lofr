d74d8cf9d455a295153b6cad34d8be83ad63adbb
Hanmant Patil <me@hnpatil.com>
2024-09-08 11:18:35 +0530

    Improves test coverage

--- handler_test.go
+++ handler_test.go
@@ -1,8 +1,15 @@
 package lofr
 
 import (
+	"bytes"
+	"context"
 	"errors"
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"gofr.dev/pkg/gofr"
+	"gofr.dev/pkg/gofr/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -67,4 +74,158 @@ func Test_validateHandler(t *testing.T) {
 			Handler(test.input)
 		})
 	}
+}
+
+func Test_QueryParams(t *testing.T) {
+	tests := []struct {
+		desc    string
+		queries map[string]interface{}
+		res     *basicRequest
+		err     error
+	}{
+		{
+			desc: "case: valid query values",
+			queries: map[string]interface{}{
+				"name":     "User Name",
+				"age":      27,
+				"score":    4.78,
+				"balance":  -100,
+				"isActive": true,
+			},
+			res: &basicRequest{
+				Name:     "User Name",
+				Age:      27,
+				Score:    4.78,
+				Balance:  -100,
+				IsActive: true,
+			},
+		},
+		{
+			desc: "case: invalid query value for uint",
+			queries: map[string]interface{}{
+				"name": "User Name",
+				"age":  "User Name",
+			},
+			err: http.ErrorInvalidParam{Params: []string{"age"}},
+		},
+		{
+			desc: "case: invalid query value for bool",
+			queries: map[string]interface{}{
+				"name":     "User Name",
+				"isActive": "User Name",
+			},
+			err: http.ErrorInvalidParam{Params: []string{"isActive"}},
+		},
+		{
+			desc: "case: invalid query value for float",
+			queries: map[string]interface{}{
+				"name":  "User Name",
+				"score": "User Name",
+			},
+			err: http.ErrorInvalidParam{Params: []string{"score"}},
+		},
+		{
+			desc: "case: invalid query value for int",
+			queries: map[string]interface{}{
+				"name":    "User Name",
+				"balance": "User Name",
+			},
+			err: http.ErrorInvalidParam{Params: []string{"balance"}},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			queryParams := url.Values{}
+
+			for k, v := range test.queries {
+				queryParams.Add(k, fmt.Sprintf("%v", v))
+			}
+
+			u := &url.URL{
+				Path: "/basic",
+			}
+
+			u.RawQuery = queryParams.Encode()
+
+			req := httptest.NewRequest("GET", u.String(), nil)
+
+			ctx := &gofr.Context{
+				Context: context.TODO(),
+				Request: http.NewRequest(req),
+			}
+
+			res, err := Handler(basicHandler)(ctx)
+			assert.Equal(t, test.err, err)
+			if err != nil {
+				assert.Nil(t, test.res)
+			} else {
+				assert.Equal(t, test.res, res)
+			}
+		})
+	}
+}
+
+func Test_RequestBody(t *testing.T) {
+	tests := []struct {
+		desc string
+		body []byte
+		res  *basicRequest
+		err  error
+	}{
+		{
+			desc: "case: valid request body",
+			body: []byte(`{"firstName":"First Name","lastName":"Last Name"}`),
+			res: &basicRequest{
+				Person: Person{
+					FirstName: "First Name",
+					LastName:  "Last Name",
+				},
+			},
+		},
+		{
+			desc: "case: invalid filed type in body",
+			body: []byte(`{"id":"First Name","lastName":"Last Name"}`),
+			err:  http.ErrorInvalidParam{Params: []string{"id"}},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/basic", bytes.NewBuffer(test.body))
+			req.Header.Set("Content-Type", "application/json")
+
+			ctx := &gofr.Context{
+				Context: context.TODO(),
+				Request: http.NewRequest(req),
+			}
+
+			res, err := Handler(basicHandler)(ctx)
+			assert.Equal(t, test.err, err)
+			if err != nil {
+				assert.Nil(t, test.res)
+			} else {
+				assert.Equal(t, test.res, res)
+			}
+		})
+	}
+}
+
+type Person struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	ID        int    `json:"id"`
+}
+
+type basicRequest struct {
+	Person
+	Name     string  `query:"name"`
+	Age      uint    `query:"age"`
+	Score    float64 `query:"score"`
+	Balance  int     `query:"balance"`
+	IsActive bool    `query:"isActive"`
+}
+
+func basicHandler(ctx *gofr.Context, req *basicRequest) (*basicRequest, error) {
+	return req, nil
 }
